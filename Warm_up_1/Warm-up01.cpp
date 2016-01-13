aec65f99e3907ab3d27d7b7cd3b83bd23854fdc5
Derek P Sifford <dereksifford@gmail.com>
2016-01-12 22:02:14 -0500

    Complete Warm Up 1

---
+++ Warm_up_1/Warm-up01.cpp
@@ -0,0 +1,13 @@
+// CS 1101Warm-up01.cpp
+// A program to demonstrate the cout object
+//
+// Programmed by: Derek P Sifford
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 11;
+    cout << "a is " << a << endl << "b is " << b << endl;
+    return 0;
+}
---
+++ Warm_up_1/hello.cpp
@@ -0,0 +1,11 @@
+// FILE:    hello.cpp
+// PURPOSE: An example of a simple I/O stream
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    return 0;
+}
